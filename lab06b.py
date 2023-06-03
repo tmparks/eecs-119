a880e72fe6e0c8af11571e7241c270f89a3cddfd
Tom Parks <tmparks@yahoo.com>
2023-06-03 15:04:04 -0400

    recursive solution to N queens

--- lab06b.py
+++ lab06b.py
@@ -14,24 +14,42 @@ def is_safe(position, queens):
     return True
 
 
+def solve_recursively(n, row, queens):
+    """
+    Attempt to place a queen in the given row of an NxN board
+    taking into account the given positions of other queens.
+    Recursively attempt to place a queen in the next row
+    and backtrack when necessary.
+    Returns true upon success.
+    """
+    for col in range(n):
+        position = (row, col)
+        if is_safe(position, queens):
+            queens.append(position) # tentatively place queen
+            if row+1 < n:
+                if solve_recursively(n, row+1, queens):
+                    return True
+                else:
+                    del queens[-1] # backtrack
+            else:
+                return True
+    return False
+
+
 def solve(n):
     """
     Attempt to place N queens on an NxN board.
-    Place each queen in the first available safe position.
-    Returns a list of up to N positions.
+    Returns a list of N positions upon success.
+    Returns an empty list upon failure.
     """
+    row = 0
     queens = list()
-    for row in range(n):
-        for col in range(n):
-            position = (row, col)
-            if is_safe(position, queens):
-                queens.append(position)
-                break # cannot place additional queens in same row
+    solve_recursively(n, row, queens)
     return queens
 
 
 def test():
-    for n in range(1, 8):
+    for n in range(1, 9):
         queens = solve(n)
         print('placed', len(queens), 'queens on', n, 'x', n, 'board')
         print(queens)
