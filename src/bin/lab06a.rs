ff2692c1e0acb32b956c3adb8e74f65145235e32
Tom Parks <tmparks@yahoo.com>
2025-06-01 01:20:42 +0000

    part 1: avoid "as usize"

--- src/bin/lab06a.rs
+++ src/bin/lab06a.rs
@@ -34,25 +34,27 @@ fn main() {
 
 /// Sort a collection of numbers.
 fn sort(numbers: &mut Vec<f64>) {
-    quicksort(numbers, 0, (numbers.len() - 1) as isize);
+    quicksort(numbers, 0, numbers.len() - 1);
 }
 
 /// Recursively sort a collection of numbers using the Quicksort algorithm.
-fn quicksort(numbers: &mut Vec<f64>, i: isize, j: isize) {
+fn quicksort(numbers: &mut Vec<f64>, i: usize, j: usize) {
     let mut left = i;
     let mut right = j;
-    let pick = numbers[i as usize];
+    let pick = numbers[i];
     while left <= right {
-        while numbers[left as usize].total_cmp(&pick) == Ordering::Less {
+        while numbers[left].total_cmp(&pick) == Ordering::Less {
             left += 1; // adjust left until an element is out of place
         }
-        while numbers[right as usize].total_cmp(&pick) == Ordering::Greater {
+        while numbers[right].total_cmp(&pick) == Ordering::Greater {
             right -= 1; // adjust right until an element is out of place
         }
         if left <= right {
-            numbers.swap(left as usize, right as usize);
+            numbers.swap(left, right);
             left += 1;
-            right -= 1;
+            if 0 < right {
+                right -= 1; // avoid underflow
+            }
         }
     }
     // recurse on smaller problems
