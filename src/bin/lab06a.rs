6d321e51b7a50655329e3e1235fa45957c1464db
Tom Parks <tmparks@yahoo.com>
2025-06-01 00:37:56 +0000

    part 1: compare result to expected

--- src/bin/lab06a.rs
+++ src/bin/lab06a.rs
@@ -1,53 +1,65 @@
 //! Programming Assignment 6: Quicksort
 
+use std::cmp::Ordering;
+use std::iter::zip;
+
 /// Demonstrate sorting.
 fn main() {
     for size in 1..30 {
-        let mut numbers = Vec::new();
-        numbers.resize(size, size as f64);
+        let mut input = Vec::new();
+        input.resize(size, size as f64);
         for i in 1..size {
-            // TODO: use random numbers
-            numbers[i] = (i as f64) - numbers[i - 1];
+            input[i] = (i as f64) - input[i - 1];
         }
-        let mut result = numbers.clone();
+        if size > 5 {
+            // some special values
+            input[0] = f64::NAN;
+            input[1] = f64::INFINITY;
+            input[2] = 0.0;
+            input[3] = -0.0;
+            input[4] = f64::NEG_INFINITY;
+            input[5] = -f64::NAN;
+        }
+        let mut expected = input.clone();
+        expected.sort_by(|a, b| a.total_cmp(b));
+        let mut result = input.clone();
         sort(&mut result);
-        println!(" input: {numbers:?}");
-        println!("result: {result:?}");
+        if !zip(&result, &expected).all(|pair| pair.0.total_cmp(&pair.1) == Ordering::Equal) {
+            println!("   input: {input:?}");
+            println!("  result: {result:?}");
+            println!("expected: {expected:?}");
+        }
     }
 }
 
 /// Sort a collection of numbers.
 fn sort(numbers: &mut Vec<f64>) {
-    quicksort(numbers, 0, numbers.len() - 1);
+    quicksort(numbers, 0, (numbers.len() - 1) as isize);
 }
 
 /// Recursively sort a collection of numbers using the Quicksort algorithm.
-fn quicksort(numbers: &mut Vec<f64>, i: usize, j: usize) {
-    let i = i as isize; // use signed integers internally
-    let j = j as isize;
+fn quicksort(numbers: &mut Vec<f64>, i: isize, j: isize) {
     let mut left = i;
     let mut right = j;
     let pick = numbers[i as usize];
     while left <= right {
-        while numbers[left as usize] < pick {
+        while numbers[left as usize].total_cmp(&pick) == Ordering::Less {
             left += 1; // adjust left until an element is out of place
         }
-        while numbers[right as usize] > pick {
+        while numbers[right as usize].total_cmp(&pick) == Ordering::Greater {
             right -= 1; // adjust right until an element is out of place
         }
         if left <= right {
-            let temp = numbers[left as usize]; // interchange elements
-            numbers[left as usize] = numbers[right as usize];
-            numbers[right as usize] = temp;
+            numbers.swap(left as usize, right as usize);
             left += 1;
             right -= 1;
         }
     }
     // recurse on smaller problems
     if i < right {
-        quicksort(numbers, i as usize, right as usize);
+        quicksort(numbers, i, right);
     }
     if left < j {
-        quicksort(numbers, left as usize, j as usize);
+        quicksort(numbers, left, j);
     }
 }
