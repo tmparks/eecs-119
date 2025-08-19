87601f1474e6d924c6b8d8ace73ebe8d1a31197d
Tom Parks <tmparks@yahoo.com>
2025-08-19 02:05:54 +0000

    human cleanup

--- lab02b.cpp
+++ lab02b.cpp
@@ -6,7 +6,7 @@ quantity:
 (1) I(x) =  ∫ exp(-t^2) dt
             0
 
-where x is beiween 0 and 1. If you do not understand this expression, don't
+where x is between 0 and 1. If you do not understand this expression, don't
 panic! You do not really have to understand what it is in order to do this
 assignment. The important thing to notice is that I(x) cannot be computed
 directly ("in closed form" as mathematicians would say). Instead, I(x) can be
@@ -33,21 +33,20 @@ and so on. It is easy to show that if only terms for n=0 through n=j are
 computed and added, the magnitude error in compating I(x) will be less than the
 value of the jth term. In other words, we do not have to compute "all" terms in
 equation (2), but can stop when we have a "small enough" term in the series (and
-thus a "sanill enough" error in the computation).
+thus a "small enough" error in the computation).
 
-Your job is to write a
-program that reads in x, computes I(x) using equation (2), and writes the
-result. Your program should do the following:
+Your job is to write a program that reads in x, computes I(x) using equation (2),
+and writes the result. Your program should do the following:
 
 (1) Check that x > 0 and that x <= 1. (If it's not, your friendly user interface
 should remind the user of the proper use of this program.)
 
-(2) Stop the computation of I(x) when term number n is between - 1E-6 and 1E-6,
-or after 15 iterations, whichever comes first. When it stops, the programn
+(2) Stop the computation of I(x) when term number n is between -1E-6 and 1E-6,
+or after 15 iterations, whichever comes first. When it stops, the program
 should print a message including the reason for stopping, the computed value of
 I(x), and the error (i.e., the value of the last term computed).
 
-(3) For term n in the series, you wil need n! x^(2n+1). To compute these values,
+(3) For term n in the series, you will need n! x^(2n+1). To compute these values,
 be sure to use the values you obtained in the previous step. That is, n! can be
 computed as n times (n-1)!, and x^(2n+1) can be computed as x times x times
 x^(2(n-1)+1).
