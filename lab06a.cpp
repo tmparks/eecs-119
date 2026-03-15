e96c99855e35d98b7b07db39b48b2df87d25ae00
Tom Parks <tmparks@yahoo.com>
2026-03-15 20:27:59 +0000

    third attempt

--- lab06a.cpp
+++ lab06a.cpp
@@ -3,6 +3,10 @@
 
 //* Fix the bugs in the file lab06a.cpp
 
+//* Remove unused code in the file lab06a.cpp
+
+//* Apply the almost-always-auto style to the program in the file lab06a.cpp
+
 #include <iostream>
 #include <vector>
 
@@ -10,33 +14,25 @@ using namespace std;
 
 const int MAX_SIZE = 15;
 
-void swap_elements(vector<int>& n, int& left, int& right) {
-    int temp = n[left];
-    n[left] = n[right];
-    n[right] = temp;
-    left++;
-    right--;
-}
-
 void quicksort(vector<int>& n, int low, int high) {
     if (low < high) {
         // Lomuto partition: choose pivot as n[high]
-        int pivot = n[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
+        auto pivot = n[high];
+        auto i = low - 1;
+        for (auto j = low; j < high; ++j) {
             if (n[j] <= pivot) {
-                i++;
+                ++i;
                 // Simple swap without modifying indices
-                int temp = n[i];
+                auto temp = n[i];
                 n[i] = n[j];
                 n[j] = temp;
             }
         }
         // Swap pivot to correct position
-        int temp = n[i + 1];
+        auto temp = n[i + 1];
         n[i + 1] = n[high];
         n[high] = temp;
-        int pi = i + 1;
+        auto pi = i + 1;
         quicksort(n, low, pi - 1);
         quicksort(n, pi + 1, high);
     }
@@ -51,14 +47,14 @@ int main() {
         cout << "Invalid size" << endl;
         return 1;
     }
-    vector<int> n(arraysize);
+    auto n = vector<int>(arraysize);
     cout << "now enter the elements of your array, all on one line " << endl;
-    for (int k = 0; k < arraysize; k++) {
+    for (auto k = 0; k < arraysize; ++k) {
         cin >> n[k];
     }
     quicksort(n, 0, arraysize - 1);
     cout << "sorted numbers are " << endl;
-    for (int k = 0; k < arraysize; k++) {
+    for (auto k = 0; k < arraysize; ++k) {
         cout << n[k] << " ";
     }
     cout << endl;
